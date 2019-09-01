f032bff24bc367f73bff2738751ca80394b26db2
Eric Johnson <ericjohnson0216@icloud.com>
2019-09-01 02:37:37 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -59,6 +59,8 @@ end
 
 def adding
   shipping_manifest
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
   # updated = shipping_manifest["porcelain vases"] = 2
   # updated["muskets"] = 2
   # updated["gun powder"] = 4
